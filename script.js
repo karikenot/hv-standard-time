0dcba2d9c971779d967ad88609d469b090113cb6
kari <iwanfadill37@gmail.com>
2025-05-02 14:29:31 +0800

    updating clock

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+function startTime() {
 const days = ["Desember", "Januari", "Februari", "Maret", "April", "Mei", "Juni"];
 const d = new Date();
 let date = d.getUTCHours();
@@ -6,14 +7,14 @@ document.getElementById("hvdate").innerHTML = "Tanggal: " + (date + 7) + " " + m
 
 let hour = d.getMinutes(); // hourcal artinya calculated hour
 hourCal = hour / 2.5
-let num = hourCal;
-let hourNod = Math.trunc(num); //hournod itu hourcal yang sudah dihapus desimalnya
+let h = Math.trunc(hourCal); //h itu hourcal yang sudah dihapus desimalnya
 
-let minute = d.getSeconds();
-hourNod = checkTime(hourNod);
-minute = checkTime(minute);
-document.getElementById("hvtime").innerHTML = "Pukul: " + hourNod + ":" + minute;
+let m = d.getSeconds();
+h = checkTime(h);
+m = checkTime(m);
+document.getElementById("hvtime").innerHTML = "Pukul: " + h + ":" + m;
 setTimeout(startTime, 1000);
+}
   
 function checkTime(i) {
     if (i < 10) {i = "0" + i};  //nambahin nol nilai digit 1
